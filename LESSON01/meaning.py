bc144bd4c58c29d1d92636799ac4a2cd89c2180a
Travis <travisjb0@gmail.com>
2023-09-20 21:50:48 -0700

    Refactor by ternary

--- LESSON01/meaning.py
+++ LESSON01/meaning.py
@@ -1,9 +1,22 @@
 meaning = 42 
 print('')
 
-if meaning > 10: 
-    print('Right on!')
-else: 
-    print('Not today')
+
+
+
+
+
+
+
+# if meaning > 10: 
+#     print('Right on!')
+# else: 
+#     print('Not today')
+
+
+# ternary operator
+
+print('Right on!') if meaning > 10 else print('Not today')
+
 
 
