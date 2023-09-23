1e051ecca4e27ed8c533d701f3196ae5f02e79a0
Travis <travisjb0@gmail.com>
2023-09-22 22:36:41 -0700

    Learned user input, control flow, etc.

---
+++ LESSON01/rps.py
@@ -0,0 +1,43 @@
+import sys
+import random
+from enum import Enum
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+
+print("")
+playerchoice= input("Enter...\n1 for Rock, \n2 for Paper, or \n3 for Scissors:\n\n")
+
+
+player = int(playerchoice)
+
+if player < 1 or player > 3: 
+    sys.exit("You must enter 1, 2, or 3.")
+
+computerchoice = random.choice("123")
+
+computer = int(computerchoice)
+
+print("")
+print("You chose " + str(RPS(player)).replace('RRPS.','') + ".")
+print("Python chose " + str(RPS(computer)).replace('RPS.', '') + ".")
+print("")
+
+if player == 1 and computer == 3:
+    print("🙌🙌🙌 You win!")
+
+elif player == 2 and computer == 1:
+    print("🙌🙌🙌 You win!")
+    
+elif player == 3 and computer == 2:
+    print("🙌🙌🙌 You win!")
+    
+elif player == computer:
+    print("😲😲😲 Draw!")
+
+else: 
+    print("🐍 Python wins!")
+
