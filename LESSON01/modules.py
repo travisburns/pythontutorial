305c55d1a15d9898be89046206782a50b0570b58
Travis <travisjb0@gmail.com>
2023-09-27 22:51:40 -0700

    Commited note that indicates that code will not run due to no module kansas

--- LESSON01/modules.py
+++ LESSON01/modules.py
@@ -3,12 +3,14 @@ from math import pi
 import sys
 import random as rdm
 from enum import Enum 
+import kansas 
 
 print(pi)
 
 for item in dir(rdm):
     print(item)
 
+print(kansas.capital)
+kansas.randomfact
 
-
-
+#code wont work right but given as example
