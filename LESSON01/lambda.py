52da22b9c2cedc31b9bfeb526775dba5c902c00c
Travis <travisjb0@gmail.com>
2023-09-28 22:44:53 -0700

    Higher order functions practiced

--- LESSON01/lambda.py
+++ LESSON01/lambda.py
@@ -21,3 +21,37 @@ addTwenty = funcBuilder(20)
 
 print(addTen(7))
 print(addTwenty(7))
+
+
+
+
+numbers = [3, 7, 12, 18, 20, 21]
+
+squared_nums = map(lambda num : num * num, numbers )
+
+print(list(squared_nums))
+
+
+##########################################
+
+
+
+odd_nums = filter(lambda num : num % 2 != 0, numbers )
+
+print(list(odd_nums))
+
+
+
+
+
+
+
+from functools import reduce 
+
+
+
+numbers = [1,2,3,4,5,1]
+
+total = reduce(lambda acc, curr: acc + curr, numbers)
+
+print(total)
